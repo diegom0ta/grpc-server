2f6444cca8f7b0844af40fe0b302b9bc001e7360
diegom0ta <motadiego98@gmail.com>
2024-01-16 17:30:46 -0300

    Setup graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"github.com/diegom0ta/grpc-server/pb"
 	"github.com/diegom0ta/grpc-server/service/user"
@@ -10,9 +16,16 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+var (
+	port = 1531
+	wg   = sync.WaitGroup{}
+)
+
 func main() {
-	port := "1531"
-	lis, err := net.Listen("tcp", ":"+port)
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -23,8 +36,25 @@ func main() {
 
 	reflection.Register(s)
 
+	sigCh := make(chan os.Signal, 1)
+
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	wg.Add(1)
+	go func() {
+		sig := <-sigCh
+		log.Printf("got signal %v, attempting graceful shutdown", sig)
+		cancel()
+		s.GracefulStop()
+
+		wg.Done()
+	}()
+
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	wg.Wait()
+	log.Println("clean shutdown")
 }
